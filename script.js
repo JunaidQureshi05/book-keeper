41c2f55455d7c763cf724ad122ef3a7ccbca118b
Junaid Qureshi <Junaid.562000@gmail.com>
2021-04-13 11:17:44 +0530

    JavaScript fuctionality added

--- script.js
+++ script.js
@@ -6,21 +6,19 @@ const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
 const bookmarksContainer = document.getElementById('bookmarks-container');
 
-// Show Modal,focus on Input
+let bookmarks = [];
 
+// Show Modal, Focus on Input
 function showModal() {
   modal.classList.add('show-modal');
   websiteNameEl.focus();
 }
 
-// Modal Event Linsteners
-
+// Modal Event Listeners
 modalShow.addEventListener('click', showModal);
-
-modalClose.addEventListener('click', () => {
-  modal.classList.remove('show-modal');
-});
-
+modalClose.addEventListener('click', () =>
+  modal.classList.remove('show-modal')
+);
 window.addEventListener('click', (e) =>
   e.target === modal ? modal.classList.remove('show-modal') : false
 );
@@ -41,17 +39,101 @@ function validate(nameValue, urlValue) {
   return true;
 }
 
-// Handle data from form
+// Build Bookmarks
+function buildBookmarks() {
+  // Remove all bookmark elements
+  bookmarksContainer.textContent = '';
+  // Build items
+  bookmarks.forEach((bookmark) => {
+    const { name, url } = bookmark;
+    // Item
+    const item = document.createElement('div');
+    item.classList.add('item');
+    // Close Icon
+    const closeIcon = document.createElement('i');
+    closeIcon.classList.add('fas', 'fa-times');
+    closeIcon.setAttribute('title', 'Delete Bookmark');
+    closeIcon.setAttribute('onclick', `deleteBookmark('${url}')`);
+    // Favicon / Link Container
+    const linkInfo = document.createElement('div');
+    linkInfo.classList.add('name');
+    // Favicon
+    const favicon = document.createElement('img');
+    favicon.setAttribute(
+      'src',
+      `https://s2.googleusercontent.com/s2/favicons?domain=${url}`
+    );
+    favicon.setAttribute('alt', 'Favicon');
+    // Link
+    const link = document.createElement('a');
+    link.setAttribute('href', `${url}`);
+    link.setAttribute('target', '_blank');
+    link.textContent = name;
+    // Append to bookmarks container
+    linkInfo.append(favicon, link);
+    item.append(closeIcon, linkInfo);
+    bookmarksContainer.appendChild(item);
+  });
+}
 
-function storeBookMark(e) {
+// Fetch bookmarks
+function fetchBookmarks() {
+  // Get bookmarks from localStorage if available
+  if (localStorage.getItem('bookmarks')) {
+    bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  } else {
+    // Create bookmarks array in localStorage
+    bookmarks = [
+      {
+        name: 'Jacinto Design',
+        url: 'http://jacinto.design',
+      },
+    ];
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  }
+  buildBookmarks();
+}
+
+// Delete Bookmark
+function deleteBookmark(url) {
+  // Loop through the bookmarks array
+  bookmarks.forEach((bookmark, i) => {
+    if (bookmark.url === url) {
+      bookmarks.splice(i, 1);
+    }
+  });
+  // Update bookmarks array in localStorage, re-populate DOM
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  fetchBookmarks();
+}
+
+function storeBookmark(e) {
   e.preventDefault();
   const nameValue = websiteNameEl.value;
   let urlValue = websiteUrlEl.value;
-  if (!urlValue.includes('https://', 'http://')) {
+  // Add 'https://' if not there
+  if (!urlValue.includes('https://') && !urlValue.includes('http://')) {
     urlValue = `https://${urlValue}`;
   }
-  validate(nameValue, urlValue);
+  // Validate
+  if (!validate(nameValue, urlValue)) {
+    return false;
+  }
+  // Set bookmark object, add to array
+  const bookmark = {
+    name: nameValue,
+    url: urlValue,
+  };
+  bookmarks.push(bookmark);
+  // Set bookmarks in localStorage, fetch, reset input fields
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  fetchBookmarks();
+  bookmarkForm.reset();
+  websiteNameEl.focus();
 }
 
 // Event Listener
-bookmarkForm.addEventListener('submit', storeBookMark);
+bookmarkForm.addEventListener('submit', storeBookmark);
+
+// On Load, Fetch Bookmarks
+fetchBookmarks();
